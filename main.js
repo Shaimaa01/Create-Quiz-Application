f64a1af14944170d617f430a74e92b9c4ad1ccd0
shaimaa <shisko939@gmail.com>
2024-10-26 21:42:32 +0200

    done

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let countSpan = document.querySelector(".quiz-info .count span");
+let bullets = document.querySelector(".bullets");
 let bulletsSpanContainer = document.querySelector(".bullets .spans");
 let quizArea = document.querySelector(".quiz-area");
 let answersArea = document.querySelector(".answers-area");
 let submitButton = document.querySelector(".submit-button");
+let resultsContainer = document.querySelector(".results");
+let countdownElement = document.querySelector(".countdown");
 
 //Set Options
 let currentIndex = 0;
 let rightAnswers = 0;
+let countdownInterval;
 
 function getQuestions() {
   let myRequest = new XMLHttpRequest();
@@ -19,6 +23,8 @@ function getQuestions() {
       createBullets(qCount);
       // Add Question Data
       addQuestionData(questionsObject[currentIndex], qCount);
+      // Start CountDown
+      countdown(5, qCount);
       // Click On Sumbit
       submitButton.onclick = function () {
         // Get Right Answer
@@ -34,6 +40,11 @@ function getQuestions() {
         addQuestionData(questionsObject[currentIndex], qCount);
         // Handle Bullets Class
         handleBullets();
+
+        // Start CountDown
+        clearInterval(countdownInterval);
+        countdown(5, qCount);
+
         // Show Results
         showResults(qCount);
       };
@@ -136,11 +147,43 @@ function handleBullets() {
   });
 }
 
+function showResults(count) {
+  let theResults;
+  if (currentIndex === count) {
+    quizArea.remove();
+    answersArea.remove();
+    submitButton.remove();
+    bullets.remove();
 
-function showResults(count){
-let theResults;
-  if(currentIndex === count){
-  quizArea.remove();
-  answersArea.remove();
+    if (rightAnswers > count / 2 && rightAnswers < count) {
+      theResults = `<span class="good">Good</span> , ${rightAnswers} From ${count} Is Good.`;
+    } else if (rightAnswers === count) {
+      theResults = `<span class="perfect">Perfect</span> , ${rightAnswers} From ${count} Is Perfect.`;
+    } else {
+      theResults = `<span class="bad">Bad</span> , ${rightAnswers} From ${count} Is So Bad.`;
+    }
+    resultsContainer.innerHTML = theResults;
+    resultsContainer.style.padding = "10px";
+    resultsContainer.style.backgroundColor = "white";
+    resultsContainer.style.marginTop = "10px";
+  }
 }
+
+function countdown(duration, count) {
+  if (currentIndex < count) {
+    let minutes, seconds;
+    countdownInterval = setInterval(function () {
+      minutes = parseInt(duration / 60);
+      seconds = parseInt(duration % 60);
+
+      minutes = minutes < 10 ? `0${minutes}` : minutes;
+      seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+      countdownElement.innerHTML = `${minutes}:${seconds}`;
+      if (--duration < 0) {
+        clearInterval(countdownInterval);
+        submitButton.click();
+      }
+    }, 1000);
+  }
 }
